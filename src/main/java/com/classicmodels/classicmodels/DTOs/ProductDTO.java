e1946de8f13f53531aca786d460904038b008ff0
Kendi-prog <muriungikendileila@gmail.com>
2025-07-29 19:42:34 +0300

    creating the product DTO

---
+++ src/main/java/com/classicmodels/classicmodels/DTOs/ProductDTO.java
@@ -0,0 +1,19 @@
+package com.classicmodels.classicmodels.DTOs;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private String productCode;
+    private String productName;
+    private String productLine;
+    private Short quantityInStock;
+    private BigDecimal buyPrice;
+}
--- src/main/java/com/classicmodels/classicmodels/service/product/ProductServiceImplementation.java
+++ src/main/java/com/classicmodels/classicmodels/service/product/ProductServiceImplementation.java
@@ -1,5 +1,6 @@
 package com.classicmodels.classicmodels.service.product;
 
+import com.classicmodels.classicmodels.DTOs.ProductDTO;
 import com.classicmodels.classicmodels.entities.Product;
 import com.classicmodels.classicmodels.repository.ProductRepository;
 import com.classicmodels.classicmodels.service.product.ProductService;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +18,17 @@ public class ProductServiceImplementation implements ProductService {
     private final ProductRepository productRepository;
 
     @Override
-    public List<Product> findAll() {
-        return productRepository.findAll();
+    public List<ProductDTO> findAll() {
+        return productRepository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+
     }
 
     @Override
-    public Optional<Product> findById(String productCode) {
-        return productRepository.findById(productCode);
+    public Optional<ProductDTO> findById(String productCode) {
+        return productRepository.findById(productCode)
+                .map(this::convertToDTO);
     }
 
     @Override
